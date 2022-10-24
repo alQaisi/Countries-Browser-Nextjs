d25b2fb9217b7c8cb62c6eac99717b9fa39fbf2f
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-10-24 15:55:16 +0300

    installing packages & configration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  images:{
+    domains:["upload.wikimedia.org","flagcdn.com"]
+  }
 }
 
 module.exports = nextConfig
