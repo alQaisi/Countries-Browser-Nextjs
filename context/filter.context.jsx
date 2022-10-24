e6002d41a6d300c29d47b7ad00c06b9d5ab5cc9f
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-10-24 15:53:59 +0300

    managing state

---
+++ context/filter.context.jsx
@@ -0,0 +1,27 @@
+import { createContext,useEffect,useContext,useState } from "react";
+import { useRouter } from "next/router";
+
+const FilterContext=createContext({});
+
+export function FilterProvider({children}){
+    const router=useRouter();
+    const [continent,setContinent]=useState("");
+    const [visited,setVisited]=useState(false);
+    const [searchText,setSearchText]=useState("");
+
+    useEffect(()=>{
+        if(!visited && continent){
+            router.push(`/continent/${continent}`)
+        }
+    },[continent]);
+
+    const value={searchText,setSearchText,continent,setContinent,setVisited};
+
+    return (
+        <FilterContext.Provider value={value}>{children}</FilterContext.Provider>
+    );
+}
+
+export function useFilterContext() {
+    return useContext(FilterContext);
+}
