082b3cc284ed7dd9aa017f4ac8d995aab574c7ca
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-11-25 22:49:38 +0200

    PWA support

--- pages/_document.js
+++ pages/_document.js
@@ -2,11 +2,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang='en'>
       <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Countries Browser Web Application"
+        />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json"/>
         <link rel='apple-touch-icon' href='/apple-touch-icon.png'/>
         <meta name='theme-color' content='#fff'/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
       </Head>
       <body>
         <Main />
