8f951fdb487c9cbece8f01c10ee2d740ed4696ec
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-12-08 20:25:07 +0200

    adding offline page

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -1,9 +1,23 @@
 import { NextPage } from "next"
+import { useThemeContext } from "../context/Theme.context";
+import styles from "../styles/Offline.module.scss";
+import homeStyles from "../styles/Home.module.scss";
+import { cls } from "../lib/cls";
+import Image from "next/image";
 
-const Offline:NextPage<{a:number}>=()=>{
+const Offline:NextPage=()=>{
+    const { theme }=useThemeContext();
     return  (
-        <div>
-            <h1>Check your internet connection !</h1>
+        <div className={cls(homeStyles.container,{[homeStyles["dark"]]:theme==="dark"},styles.offline)}>
+            <div className={styles.svgWrapper}>
+                <div className={styles.ratio}/>
+                {
+                    theme==="dark"
+                    ?<Image src="/assets/DarkNetwork.svg" fill alt="network error"/>
+                    :<Image src="/assets/Network.svg" fill alt="network error"/>
+                }
+            </div>
+            <h1 className={theme==="dark"?styles.darkWarning:""}>Check your internet connection !</h1>
         </div>
     );
 }
