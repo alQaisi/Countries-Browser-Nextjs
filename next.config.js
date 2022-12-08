5ab97e488466b8fabe0ae739f0c3fa7385fd612d
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-12-08 19:22:23 +0200

    enable offline page

--- next.config.js
+++ next.config.js
@@ -7,8 +7,9 @@ const nextConfig = {
 module.exports = nextConfig
 
 const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
 
 module.exports = withPWA({
   experimental: {
