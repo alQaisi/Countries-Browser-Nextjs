628ae3ed8834745b540f2661322b39ab8ab92543
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-12-10 17:09:15 +0200

    creating custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/404.module.scss";
+import { useThemeContext } from "../context/Theme.context";
+import { cls } from "../lib/cls";
+import Button from "../components/atoms/button/button";
+
+const NotFoundPage:NextPage=()=>{
+    const { theme }=useThemeContext();
+    return (
+        <div className={cls(styles.Error,{[styles["dark"]]:theme==="dark"})}>
+            <Link href="/"><button>Go to Home</button></Link>
+            <div className={styles.svgWrapper}>
+                <div className={styles.ratio}/>
+                {
+                    theme==="dark"
+                    ?<Image src="/assets/404dark.svg" fill alt="404 not found"/>
+                    :<Image src="/assets/404normal.svg" fill alt="404 not found"/>
+                }
+            </div>
+        </div>
+    );
+}
+export default NotFoundPage;
